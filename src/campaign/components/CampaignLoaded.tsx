39e006387bd3df56740e509b77459d145b41a9d7
Matt Beech <matthew.beech@moonpig.com>
2021-07-09 10:24:46 +0100

    feat: add button to modify money

--- src/campaign/components/CampaignLoaded.tsx
+++ src/campaign/components/CampaignLoaded.tsx
@@ -6,6 +6,7 @@ import {
   IconButton,
   SimpleGrid,
   Text,
+  Tooltip,
 } from "@chakra-ui/react";
 import { FetchCampaign_fetchCampaign_Campaign as Campaign } from "campaign/gql";
 import React from "react";
@@ -14,6 +15,7 @@ import { AddItemModal } from "./AddItemModal";
 import { Currency } from "./Currency";
 import { ItemCard } from "./ItemCard";
 import { MoneyModal } from "./MoneyModal";
+import { FaPiggyBank } from "react-icons/Fa";
 
 export const CampaignLoaded = ({ campaign }: { campaign: Campaign }) => {
   const { openModal } = useModal();
@@ -23,26 +25,36 @@ export const CampaignLoaded = ({ campaign }: { campaign: Campaign }) => {
         <Text fontSize="xl">{campaign.name}</Text>
 
         <Box ml="auto">
-          <IconButton
-            aria-label="add item"
-            variant="ghost"
-            size="lg"
-            icon={<AddIcon />}
-            onClick={() => openModal(<AddItemModal campaignId={campaign.id} />)}
-          />
+          <Tooltip label="add item">
+            <IconButton
+              aria-label="add item"
+              variant="ghost"
+              size="lg"
+              icon={<AddIcon />}
+              onClick={() =>
+                openModal(<AddItemModal campaignId={campaign.id} />)
+              }
+            />
+          </Tooltip>
         </Box>
       </Flex>
 
-      <Flex
-        mb={4}
-        cursor="pointer"
-        onClick={() => openModal(<MoneyModal campaignId={campaign.id} />)}
-      >
+      <Flex mb={4}>
         <Currency denomination="platinum" value={campaign.platinum} mr={3} />
         <Currency denomination="gold" value={campaign.gold} mr={3} />
         <Currency denomination="electrum" value={campaign.electrum} mr={3} />
         <Currency denomination="silver" value={campaign.silver} mr={3} />
-        <Currency denomination="copper" value={campaign.copper} mr={3} />
+        <Currency denomination="copper" value={campaign.copper} mr={2} />
+
+        <Tooltip label="modify money">
+          <IconButton
+            aria-label="edit money"
+            variant="ghost"
+            size="lg"
+            icon={<FaPiggyBank />}
+            onClick={() => openModal(<MoneyModal campaignId={campaign.id} />)}
+          />
+        </Tooltip>
       </Flex>
 
       {campaign.items.length === 0 ? (
