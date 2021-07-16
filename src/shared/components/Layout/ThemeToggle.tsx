a551e38ba2000960f19f3031a3aa91060b87d004
Matt Beech <matthew.beech@moonpig.com>
2021-07-16 13:04:07 +0100

    test: add theme toggle test

--- src/shared/components/Layout/ThemeToggle.tsx
+++ src/shared/components/Layout/ThemeToggle.tsx
@@ -10,7 +10,13 @@ const ThemeToggle = () => {
       aria-label="theme toggle"
       variant="ghost"
       size="lg"
-      icon={colorMode === "light" ? <RiMoonFill /> : <RiSunLine />}
+      icon={
+        colorMode === "light" ? (
+          <RiMoonFill data-testid="moon-icon" />
+        ) : (
+          <RiSunLine data-testid="sun-icon" />
+        )
+      }
       onClick={toggleColorMode}
     />
   );
