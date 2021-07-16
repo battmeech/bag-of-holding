29dce1c92d858cf4ad29c02e16ae3dd82e7e8733
Matt Beech <matthew_beech@live.co.uk>
2021-07-16 15:21:46 +0100

    chore: remove test coverage thresholds

--- jest.config.js
+++ jest.config.js
@@ -4,13 +4,5 @@ module.exports = {
   moduleDirectories: ["node_modules", "src"],
   clearMocks: true,
   collectCoverageFrom: ["src/**/*.{js,ts,tsx}", "!src/**/__test__/**"],
-  coverageThreshold: {
-    global: {
-      branchs: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100,
-    },
-  },
   testEnvironment: "jsdom",
 };
