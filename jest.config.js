8457084bfdab84dfa28b2db275c38964284f1ada
Matt Beech <matthew.beech@moonpig.com>
2021-07-23 17:59:44 +0100

    chore: remove coverage from stories

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,11 @@ module.exports = {
   testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
   moduleDirectories: ["node_modules", "src"],
   clearMocks: true,
-  collectCoverageFrom: ["src/**/*.{js,ts,tsx}", "!src/**/__test__/**"],
+  collectCoverageFrom: [
+    "src/**/*.{js,ts,tsx}",
+    "!src/**/__test__/**",
+    "!src/**/*.stories.*",
+    "!src/shared/generators/*",
+  ],
   testEnvironment: "jsdom",
 };
