58ded3603ba6c62bce091ef8daae3f9087ade6d9
Matt Beech <matthew.beech@moonpig.com>
2021-08-18 11:23:05 +0100

    feat: add sign in and sign out buttons

--- src/shared/components/Layout/UserAvatar.tsx
+++ src/shared/components/Layout/UserAvatar.tsx
@@ -1,4 +1,5 @@
-import { Avatar, Menu, MenuButton } from "@chakra-ui/react";
+import { Avatar, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import { signIn, signOut } from "next-auth/client";
 import React from "react";
 import { WithMaybeSession } from "types";
 
@@ -6,8 +7,17 @@ export const UserAvatar: React.FC<WithMaybeSession> = ({ session }) => {
   return (
     <Menu>
       <MenuButton>
-        <Avatar size="sm" src={session?.user.image} />
+        <Avatar size="sm" src={session?.user?.image} />
       </MenuButton>
+      <MenuList>
+        {session?.user ? (
+          <MenuItem onClick={() => signOut({ callbackUrl: "/" })}>
+            sign out
+          </MenuItem>
+        ) : (
+          <MenuItem onClick={() => signIn()}>sign in</MenuItem>
+        )}
+      </MenuList>
     </Menu>
   );
 };
