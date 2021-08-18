6119d48659bfcd9fb9824a4d3dcf0bd84e77d748
Dan Austin <dan.austin@hey.com>
2021-08-18 19:05:51 +0100

    feat: show loading and no-data states on campaigns and items

--- src/campaigns/Campaigns.tsx
+++ src/campaigns/Campaigns.tsx
@@ -1,6 +1,15 @@
 import { useQuery } from "@apollo/client";
 import { AddIcon } from "@chakra-ui/icons";
-import { Box, Flex, IconButton, Text, Tooltip } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Center,
+  Flex,
+  IconButton,
+  Text,
+  Tooltip,
+  VStack,
+} from "@chakra-ui/react";
 import React from "react";
 import { useModal } from "shared";
 import { CampaignList } from "./components/CampaignList";
@@ -8,9 +17,8 @@ import { CampaignModal } from "./components/CampaignModal";
 import { ListCampaigns, ListCampaignsGQL } from "./gql";
 
 export const Campaigns = () => {
-  const { data } = useQuery<ListCampaigns>(ListCampaignsGQL);
+  const { data, loading } = useQuery<ListCampaigns>(ListCampaignsGQL);
   const { openModal } = useModal();
-
   return (
     <Box>
       <Flex as="header" width="full" align="center">
@@ -28,8 +36,21 @@ export const Campaigns = () => {
           </Tooltip>
         </Box>
       </Flex>
-
-      <CampaignList campaigns={data?.campaigns} />
+      {data && data.campaigns.length < 1 ? (
+        <Center w="full" h="60vh">
+          <VStack spacing="8">
+            <Text>nothing to see here!</Text>
+            <Button
+              colorScheme="teal"
+              onClick={() => openModal(<CampaignModal />)}
+            >
+              create a campaign
+            </Button>
+          </VStack>
+        </Center>
+      ) : (
+        <CampaignList loading={loading} campaigns={data?.campaigns} />
+      )}
     </Box>
   );
 };
