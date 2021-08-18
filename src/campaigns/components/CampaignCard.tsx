07241b720dc65d4401881928186e77e86f0847f5
Matt Beech <matthew_beech@live.co.uk>
2021-08-18 16:27:28 +0100

    feat: campaign card styling adjustments

--- src/campaigns/components/CampaignCard.tsx
+++ src/campaigns/components/CampaignCard.tsx
@@ -1,35 +1,58 @@
-import { HStack, Link, Text, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  chakra,
+  Divider,
+  Heading,
+  HStack,
+  Link,
+  Stack,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import { ListCampaigns_campaigns as Campaign } from "campaigns/gql";
 import NextLink from "next/link";
+import React from "react";
 import { GiElfHelmet, GiPlainDagger } from "react-icons/gi";
 
 export const CampaignCard = ({ campaign }: { campaign: Campaign }) => {
   return (
-    <Link as={NextLink} href={`/campaigns/${campaign.id}`}>
-      <HStack
-        cursor="pointer"
-        p="4"
-        borderWidth="1px"
-        borderRadius="lg"
-        justify="space-between"
-      >
-        <VStack>
-          <Text w="full" fontSize="md">
-            {campaign.name}
-          </Text>
-        </VStack>
+    <VStack p="4" borderWidth="1px" borderRadius="lg">
+      <Box py="2">
+        <Heading fontSize="lg" textTransform="lowercase">
+          {campaign.name}
+        </Heading>
+      </Box>
 
-        <VStack>
-          <HStack w="full" justifyContent="flex-end">
-            <Text fontSize="xs">{campaign.userCount} players</Text>
-            <GiElfHelmet />
-          </HStack>
-          <HStack w="full" justifyContent="flex-end">
-            <Text fontSize="xs">{campaign.itemCount} items</Text>{" "}
-            <GiPlainDagger />
-          </HStack>
-        </VStack>
-      </HStack>
-    </Link>
+      <Divider />
+
+      <Stack
+        spacing={{ base: "2", sm: "8" }}
+        direction={{ base: "column", sm: "row" }}
+      >
+        <HStack align="center">
+          <GiElfHelmet />
+          <Text>
+            {campaign.userCount}{" "}
+            <chakra.span color="gray.500">players</chakra.span>
+          </Text>
+        </HStack>
+        <HStack align="center">
+          <GiPlainDagger />
+          <Text>
+            {campaign.itemCount}{" "}
+            <chakra.span color="gray.500">items</chakra.span>
+          </Text>
+        </HStack>
+      </Stack>
+
+      <Box w="full">
+        <Link as={NextLink} href={`/campaigns/${campaign.id}`}>
+          <Button w="full" variant="link" colorScheme="teal">
+            view
+          </Button>
+        </Link>
+      </Box>
+    </VStack>
   );
 };
