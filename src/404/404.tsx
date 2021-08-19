b871a2459de6c6de53d39c0ca51091335a37195e
Matt Beech <matthew_beech@live.co.uk>
2021-08-19 16:59:32 +0100

    feat: update 404 image to be a d20

---
+++ src/404/404.tsx
@@ -0,0 +1,44 @@
+/* istanbul ignore file */
+import { Button } from "@chakra-ui/button";
+import { Box, Heading, Text } from "@chakra-ui/layout";
+import { useColorModeValue } from "@chakra-ui/react";
+import Image from "next/image";
+import Link from "next/link";
+import { MotionBox } from "shared";
+import darkDice from "../../public/dark-dice.png";
+import lightDice from "../../public/light-dice.png";
+
+export const Page404: React.FC = () => {
+  return (
+    <>
+      <MotionBox
+        animate={{ y: 20 }}
+        transition={{ repeat: Infinity, duration: 2, repeatType: "reverse" }}
+        width={["100%", "70%", "60%", "60%"]}
+        margin="0 auto"
+      >
+        <Image
+          src={useColorModeValue(lightDice, darkDice)}
+          alt="nat 1 dice roll"
+        />
+      </MotionBox>
+      <Text textAlign="center" fontSize="xs">
+        page not found
+      </Text>
+
+      <Box marginY={4}>
+        <Heading textAlign="center">
+          you failed your investigation check
+        </Heading>
+
+        <Box textAlign="center" marginTop={4}>
+          <Link href="/" passHref>
+            <Button mt="4" colorScheme="teal">
+              head back
+            </Button>
+          </Link>
+        </Box>
+      </Box>
+    </>
+  );
+};
