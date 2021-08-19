f6f6300233294621dc0d342d5c09e9a0b1679213
Dan Austin <dan.austin@hey.com>
2021-08-19 15:45:00 +0100

    feat: add signup form

---
+++ src/signup/components/__tests__/SignUpForm.tsx
@@ -0,0 +1,29 @@
+import { render, screen } from "shared";
+import { SignUpForm } from "../SignUpForm";
+
+jest.spyOn(require("next-auth/client"), "useSession").mockReturnValue([
+  {
+    user: {
+      image: "test-image-url",
+    },
+  },
+]);
+
+describe("SignUpForm", () => {
+  it("should render a username input", () => {
+    render(<SignUpForm />);
+    expect(screen.getByLabelText(/username/i)).toBeVisible();
+  });
+  it("should render an avatar url input", () => {
+    render(<SignUpForm />);
+    expect(screen.getByLabelText(/avatar url/i)).toBeVisible();
+  });
+  it("should render a submit button", () => {
+    render(<SignUpForm />);
+    expect(screen.getByRole("button", { name: /submit/i })).toBeVisible();
+  });
+  it("should prepopulate the avatar url with the session image URL if availavle", () => {
+    render(<SignUpForm />);
+    expect(screen.getByLabelText(/avatar url/i)).toHaveValue("test-image-url");
+  });
+});
