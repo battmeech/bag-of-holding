f0025c97591c64be5acc3f51eff48c5af510c1cd
Matt Beech <matthew_beech@live.co.uk>
2021-08-25 18:27:35 +0100

    feat: require authenticated user to created issue

--- src/pages/api/issues/bug.ts
+++ src/pages/api/issues/bug.ts
@@ -1,6 +1,14 @@
 import { createIssue } from "api";
 import { NextApiHandler } from "next";
+import { getSession } from "shared/session";
 
-const handler: NextApiHandler = (req, res) => createIssue(req, res, "bug");
+const handler: NextApiHandler = async (req, res) => {
+  const session = await getSession({ req });
+  if (!session.user)
+    res
+      .status(401)
+      .json({ message: "You must be signed in to raise an issue" });
+  else createIssue(req, res, "bug");
+};
 
 export default handler;
