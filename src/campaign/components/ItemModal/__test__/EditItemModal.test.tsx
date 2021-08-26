4f7cea505ba572e723549896d0b275b2160eb6e4
Matt Beech <matthew.beech@moonpig.com>
2021-08-26 23:53:27 +0100

    test: remove unneeded tests

--- src/campaign/components/ItemModal/__test__/EditItemModal.test.tsx
+++ src/campaign/components/ItemModal/__test__/EditItemModal.test.tsx
@@ -1,13 +1,12 @@
-import * as GQL from "@apollo/client";
 import { Modal } from "@chakra-ui/react";
-import React from "react";
-import { fireEvent, render, waitFor } from "shared";
 import { EditItemModal } from "campaign/components/ItemModal/EditItemModal";
 import { EditItem_editItem_Item as Item } from "campaign/gql";
+import React from "react";
+import { fireEvent, render, waitFor } from "shared";
 import { createItem } from "shared/testData";
 
 describe("EditItemModal", () => {
-  const setUpComponent = ({ item = createItem({}) }: { item?: Item }) => {
+  const setupComponent = ({ item = createItem({}) }: { item?: Item }) => {
     const rendered = render(
       <Modal isOpen={true} onClose={jest.fn()}>
         <EditItemModal item={item} />
@@ -17,66 +16,39 @@ describe("EditItemModal", () => {
   };
 
   it("renders a modal with close and save buttons", () => {
-    const { getByText } = setUpComponent({});
+    const { getByText } = setupComponent({});
 
     expect(getByText("close")).toBeInTheDocument();
     expect(getByText("save item")).toBeInTheDocument();
   });
 
-  it("save is enabled on modal load is entered", () => {
-    const { getByText } = setUpComponent({});
+  it("save is enabled on modal load is entered", async () => {
+    const { getByText } = setupComponent({});
 
-    expect(getByText("save item")).not.toBeDisabled();
+    await waitFor(() => {
+      expect(getByText("save item")).not.toBeDisabled();
+    });
   });
 
-  it("save is enabled when no description is entered", () => {
-    const { getByText, getByPlaceholderText } = setUpComponent({});
+  it("save is enabled when no description is entered", async () => {
+    const { getByText, getByPlaceholderText } = setupComponent({});
 
     const input = getByPlaceholderText("item description");
-
     fireEvent.change(input, { target: { value: "" } });
 
-    expect(getByText("save item")).not.toBeDisabled();
+    await waitFor(() => {
+      expect(getByText("save item")).not.toBeDisabled();
+    });
   });
 
-  it("save is disabled when no name is entered", () => {
-    const { getByText, getByPlaceholderText } = setUpComponent({});
+  it("save is disabled when no name is entered", async () => {
+    const { getByText, getByPlaceholderText } = setupComponent({});
 
     const input = getByPlaceholderText("item name");
-
     fireEvent.change(input, { target: { value: "" } });
 
-    expect(getByText("save item")).toBeDisabled();
-  });
-
-  it("pressing save sends a gql request", async () => {
-    // Setup
-    const mutateMock = jest.fn().mockResolvedValue({});
-    jest
-      .spyOn(GQL, "useMutation")
-      .mockReturnValue([mutateMock, { loading: false } as any]);
-    const { getByText, getByPlaceholderText } = setUpComponent({});
-
-    // Run
-    const input = getByPlaceholderText("item name");
-    fireEvent.change(input, { target: { value: "Test Item Name" } });
-
-    const saveButton = getByText("save item");
-    fireEvent.click(saveButton);
-
-    // Assert
-    await waitFor(() =>
-      expect(mutateMock).toHaveBeenCalledWith({
-        variables: {
-          id: "item-id",
-          input: {
-            description: "Test description",
-            name: "Test Item Name",
-            quantity: 1,
-            tags: ["tag-a", "tag-b"],
-          },
-        },
-      })
-    );
+    await waitFor(() => {
+      expect(getByText("save item")).toBeDisabled();
+    });
   });
 });
