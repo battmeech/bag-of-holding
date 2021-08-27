65d42d780a56ccdd1aea87e5643f0d7cdbf60547
Matt Beech <matthew.beech@moonpig.com>
2021-08-27 09:53:39 +0100

    refactor: extract filtering to hook

--- src/campaign/components/CampaignContent/__test__/CampaignLoaded.test.tsx
+++ src/campaign/components/CampaignContent/__test__/CampaignLoaded.test.tsx
@@ -1,8 +1,8 @@
 import userEvent from "@testing-library/user-event";
 import { FetchCampaign_campaign_Campaign as Campaign } from "campaign/gql";
 import React from "react";
-import { fireEvent, render } from "shared";
-import { createCampaign, createItem } from "shared/testData";
+import { fireEvent, render, waitFor } from "shared";
+import { createCampaign } from "shared/testData";
 import { CampaignLoaded } from "../CampaignLoaded";
 
 describe("CampaignLoaded", () => {
@@ -28,40 +28,35 @@ describe("CampaignLoaded", () => {
   });
 
   it("renders the money modification modal when clicking the piggy bank", () => {
-    // Setup
     const { getByLabelText, getByText } = setUpComponent({});
 
-    // Run
     const piggyBank = getByLabelText("edit money");
     fireEvent.click(piggyBank);
 
-    // Assert
     expect(getByText("modify money")).toBeInTheDocument();
   });
 
-  it("renders the add item modal when clicking the plus button", () => {
-    // Setup
+  it("renders the add item modal when clicking the plus button", async () => {
     const { getByLabelText, getByText } = setUpComponent({});
 
-    // Run
     const plusButton = getByLabelText("add item");
     fireEvent.click(plusButton);
 
-    // Assert
-    expect(getByText("new item")).toBeInTheDocument();
+    await waitFor(() => {
+      expect(getByText("new item")).toBeInTheDocument();
+    });
   });
 
-  it("renders the add item modal when clicking the add item button", () => {
-    // Setup
+  it("renders the add item modal when clicking the add item button", async () => {
     const campaign = { ...createCampaign(), items: [] };
     const { getByText } = setUpComponent({ campaign });
 
-    // Run
     const addItem = getByText("add an item");
     fireEvent.click(addItem);
 
-    // Assert
-    expect(getByText("new item")).toBeInTheDocument();
+    await waitFor(() => {
+      expect(getByText("new item")).toBeInTheDocument();
+    });
   });
 
   it("renders a search input when items exist on the campaign", () => {
@@ -75,91 +70,13 @@ describe("CampaignLoaded", () => {
     expect(queryByPlaceholderText(/search/i)).not.toBeInTheDocument();
   });
 
-  describe("filtering", () => {
-    const items = [
-      createItem({
-        id: "1",
-        name: "test1",
-        description: "description1",
-        tags: ["tag-1", "tag-2"],
-      }),
-      createItem({
-        id: "2",
-        name: "test2",
-        description: "description2",
-        tags: ["tag-3"],
-      }),
-      createItem({ id: "3", name: "test3", description: "description3" }),
-      createItem({ id: "4", name: "test4", description: "description4" }),
-    ];
-    it("filters items by name when searched", () => {
-      // Setup
-      const campaign = { ...createCampaign(), items };
-      const { getByText, queryByText, getByPlaceholderText } = setUpComponent({
-        campaign,
-      });
+  it("clears the filter text when the clear button is pressed", () => {
+    const { getByPlaceholderText, getByLabelText } = setUpComponent({});
 
-      // Run
-      const textbox = getByPlaceholderText(/search/i);
-      userEvent.type(textbox, "test3");
+    const textbox = getByPlaceholderText(/search for items/i);
+    userEvent.type(textbox, "filter");
+    fireEvent.click(getByLabelText(/clear filter/i));
 
-      // Assert
-      expect(getByText(/test3/)).toBeVisible();
-      expect(queryByText(/test1/)).not.toBeInTheDocument();
-      expect(queryByText(/test2/)).not.toBeInTheDocument();
-      expect(queryByText(/test4/)).not.toBeInTheDocument();
-    });
-
-    it("filters items by description when searched", () => {
-      // Setup
-      const campaign = { ...createCampaign(), items };
-      const { getByText, queryByText, getByPlaceholderText } = setUpComponent({
-        campaign,
-      });
-
-      // Run
-      const textbox = getByPlaceholderText(/search/i);
-      userEvent.type(textbox, "description2");
-
-      // Assert
-      expect(getByText(/test2/)).toBeVisible();
-      expect(queryByText(/test1/)).not.toBeInTheDocument();
-      expect(queryByText(/test3/)).not.toBeInTheDocument();
-      expect(queryByText(/test4/)).not.toBeInTheDocument();
-    });
-
-    it("filters items by tag when a tag is clicked", () => {
-      // Setup
-      const campaign = { ...createCampaign(), items };
-      const { getByText, queryByText } = setUpComponent({
-        campaign,
-      });
-
-      // Run
-      const tags = getByText("tag-1");
-      fireEvent.click(tags);
-
-      // Assert
-      expect(getByText(/test1/)).toBeVisible();
-      expect(queryByText(/test2/)).not.toBeInTheDocument();
-      expect(queryByText(/test3/)).not.toBeInTheDocument();
-      expect(queryByText(/test4/)).not.toBeInTheDocument();
-    });
-
-    it("clears the filter text when the clear button is pressed", () => {
-      // Setup
-      const campaign = { ...createCampaign(), items };
-      const { getByPlaceholderText, getByLabelText } = setUpComponent({
-        campaign,
-      });
-
-      // Run
-      const textbox = getByPlaceholderText(/search for items/i);
-      userEvent.type(textbox, "filter");
-      fireEvent.click(getByLabelText(/clear filter/i));
-
-      // Assert
-      expect(textbox).toHaveValue("");
-    });
+    expect(textbox).toHaveValue("");
   });
 });
