6541819252abbb0662b3a5ff93a7ce3bfde6de8c
Matt Beech <matthew_beech@live.co.uk>
2023-10-03 18:16:17 +0100

    feat: begin adding auth

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,11 +2,19 @@
 import { CacheProvider } from "@chakra-ui/next-js";
 import { ChakraProvider } from "@chakra-ui/react";
 import { customTheme } from "@/theme";
+import { SessionProvider } from "next-auth/react";
+import { Session } from "next-auth";
 
-export function Providers({ children }: { children: React.ReactNode }) {
-  return (
+export const Providers = ({
+  children,
+  session,
+}: {
+  children: React.ReactNode;
+  session: Session | null;
+}) => (
+  <SessionProvider session={session}>
     <CacheProvider>
       <ChakraProvider theme={customTheme}>{children}</ChakraProvider>
     </CacheProvider>
-  );
-}
+  </SessionProvider>
+);
