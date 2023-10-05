655aa6954318a201ce1a470752ea735d3fafe817
Matt Beech <matthew_beech@live.co.uk>
2023-10-05 15:35:13 +0100

    fix: broken test

--- src/ui-components/Header/Header.test.tsx
+++ src/ui-components/Header/Header.test.tsx
@@ -1,9 +1,14 @@
 import { Header } from "@ui-components/Header";
 import { render, screen } from "@testing-library/react";
+import { SessionProvider } from "next-auth/react";
 
 describe(Header.name, () => {
   it("should render the website name", async () => {
-    render(<Header />);
+    render(
+      <SessionProvider session={null}>
+        <Header />
+      </SessionProvider>
+    );
 
     expect(screen.getByText("bag of holding")).toBeInTheDocument();
   });
