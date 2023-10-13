fce8b29145ec30783c0d5d752a0999bbd035e7a3
Matt Beech <matthew_beech@live.co.uk>
2023-10-13 17:09:05 +0100

    refactor: create new components for contextual buttons

---
+++ src/ui-views/Campaign/CampaignButtonGroup/ContextualButtons.tsx
@@ -0,0 +1,78 @@
+import {
+  useViewProvider,
+  View,
+} from "@ui-views/Campaign/ViewControls/ViewProvider";
+import React, { FC, ReactNode } from "react";
+import { IconButton, MenuItem, Tooltip } from "@chakra-ui/react";
+import { AddIcon } from "@chakra-ui/icons";
+import { AddItemModal } from "@ui-views/Campaign/ItemModal/AddItemModal";
+import { AddQuestModal } from "@ui-views/Campaign/QuestModal/AddQuestModal";
+import { useModal } from "@ui-components/ModalProvider";
+
+type ContextualButtonProps = {
+  campaignId: string;
+};
+
+export const ContextualAddButton: FC<ContextualButtonProps> = ({
+  campaignId,
+}) => {
+  const { openModal } = useModal();
+  const { view } = useViewProvider();
+
+  const contextualAddButton: Record<View, ReactNode> = {
+    item: (
+      <Tooltip label="add item">
+        <IconButton
+          aria-label="add item"
+          variant="ghost"
+          size="lg"
+          icon={<AddIcon />}
+          onClick={() =>
+            openModal(<AddItemModal campaignId={campaignId} />, "md")
+          }
+        />
+      </Tooltip>
+    ),
+    quest: (
+      <Tooltip label="add quest">
+        <IconButton
+          aria-label="add quest"
+          variant="ghost"
+          size="lg"
+          icon={<AddIcon />}
+          onClick={() =>
+            openModal(<AddQuestModal campaignId={campaignId} />, "md")
+          }
+        />
+      </Tooltip>
+    ),
+  };
+
+  return contextualAddButton[view];
+};
+
+export const ContextualMenuButton: FC<ContextualButtonProps> = ({
+  campaignId,
+}) => {
+  const { openModal } = useModal();
+  const { view } = useViewProvider();
+
+  const contextualMenuItem: Record<View, ReactNode> = {
+    item: (
+      <MenuItem
+        onClick={() => openModal(<AddItemModal campaignId={campaignId} />)}
+      >
+        add item
+      </MenuItem>
+    ),
+    quest: (
+      <MenuItem
+        onClick={() => openModal(<AddQuestModal campaignId={campaignId} />)}
+      >
+        add quest
+      </MenuItem>
+    ),
+  };
+
+  return contextualMenuItem[view];
+};
